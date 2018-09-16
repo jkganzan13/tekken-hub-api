d94da067609f731e25f59da2ba7b5f524e6678f0
John Kenneth Ganzan <jkganzan13@gmail.com>
2018-09-16 20:13:39 +1200

    Remove firebase functions

--- functions/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const moves = require('./moves');
-
-const routes = express.Router();
-
-/** API Routes */
-routes.get('/moves', moves.get);
-
-module.exports = routes;
--- functions/test/web-scraper/index.js
+++
@@ -1,9 +0,0 @@
-const webscraper = require('../../web-scraper');
-
-describe.skip('web-scraper', () => {
-  it('should be successful', async () => {
-    const response = await webscraper();
-    console.log(response)
-    expect(response).to.be.ok;
-  });
-});
