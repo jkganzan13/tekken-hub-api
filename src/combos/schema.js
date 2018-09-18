4a051e293156d50578619d9dcb8b56bae589d5ab
John Kenneth Ganzan <jkganzan13@gmail.com>
2018-09-18 23:29:35 +1200

    Add timestamp to Combo schema

--- src/combos/schema.js
+++ src/combos/schema.js
@@ -1,9 +1,11 @@
 const dynamoose = require('dynamoose');
+const uuidv4 = require('uuid').v4;
 
 module.exports = () => new dynamoose.Schema({
   id: {
     type: String,
     hashKey: true,
+    default: uuidv4,
   },
   submittedBy: {
     type: String,
@@ -12,7 +14,10 @@ module.exports = () => new dynamoose.Schema({
   combo: String,
   damage: Number,
   name: String,
-  numRatings: Number,
+  numRatings: {
+    type: Number,
+    default: 0,
+  },
   ratings: {
     type: 'list',
     list: [{
@@ -20,7 +25,9 @@ module.exports = () => new dynamoose.Schema({
       upvote: Boolean,
     }]
   },
+  createdAt: Date,
 }, {
   useDocumentTypes: true,
   useNativeBooleans: true,
+  timestamps: true,
 });
