3acf26ab184eea49b7165c180786d83feab41622
John Kenneth Ganzan <jkganzan13@gmail.com>
2018-09-24 16:44:13 +1200

    Fix build error 'serverless: not found'

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,7 @@ export GIT_BRANCH=${BITBUCKET_BRANCH:-n/a}
 export GIT_COMMIT=$(git rev-parse HEAD | cut -c1-7)
 
 # debug
-echo "about to deploy for '${ENVIRONMENT}' for tag '${GIT_TAG}' on commit '${GIT_COMMIT}'"
+echo "about to deploy to '${ENVIRONMENT}' on commit '${GIT_COMMIT}'"
 
 # serverless deploy
 npm run deploy -- --stage ${ENVIRONMENT}
