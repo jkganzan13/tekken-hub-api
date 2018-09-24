41288588b3e82062fe98f47c0d285aa0c864546f
John Kenneth Ganzan <jkganzan13@gmail.com>
2018-09-24 20:04:11 +1200

    Remove jwt authorizer in this function

--- src/middleware.js
+++ src/middleware.js
@@ -1,33 +1,16 @@
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const jwt = require('express-jwt');
-const jwks = require('jwks-rsa');
+const jwt = require('jsonwebtoken');
 
-var jwtCheck = jwt({
-  secret: jwks.expressJwtSecret({
-      cache: true,
-      rateLimit: true,
-      jwksRequestsPerMinute: 5,
-      jwksUri: process.env.JWKS_URI
-  }),
-  audience: process.env.AUTH0_AUDIENCE,
-  issuer: process.env.AUTH0_ISSUER,
-  algorithms: ['RS256']
-});
-
-const authErrorHandler = (err, req, res, next) => {
-  if (err.name === 'UnauthorizedError') {
-    res.status(err.status).json({
-      status: err.status,
-      message: err.message
-    });
-  }
+const decodeToken = (req, res, next) => {
+  const token = req.headers['authorization'].split(' ')[1];
+  req.user = jwt.decode(token);
+  next();
 };
 
 // Middleware
 module.exports = (app) => {
   app.use(bodyParser.json());
   app.use(cors());
-  app.use(jwtCheck);
-  app.use(authErrorHandler);
+  app.use(decodeToken);
 };
